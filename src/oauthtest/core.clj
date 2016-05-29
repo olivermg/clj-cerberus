4e7c23d4f97f93daa6c9aa558a0cb7f6ba13b356
Oliver Wegner <void1976@gmail.com>
2016-05-29 20:54:25 +0200

    sending tokenrequest

--- src/oauthtest/core.clj
+++ src/oauthtest/core.clj
@@ -5,8 +5,6 @@
             [compojure.handler :refer [site]]))
 
 
-(defonce server (atom nil))
-
 (comment (defn get-env []
            {:clientid (System/getenv "CLIENTID")
             :clientsecret (System/getenv "CLIENTSECRET")
@@ -20,7 +18,8 @@
                                                 "http://localhost:8899/auth/code"
                                                 ["openid" "email"]))
            (GET "/code" [] (h/make-code-handler "817304564499-tg8htvvunf7ds8rbp0jc30ijm20odo7k.apps.googleusercontent.com"
-                                                "http://www.googleapis.com"
+                                                "dFlqsKkhk0oA1se3zk5AUKYJ"
+                                                "https://www.googleapis.com/oauth2/v4/token"
                                                 "http://localhost:8899/auth/code"))))
 
 (defn start []
--- src/oauthtest/handlers.clj
+++ src/oauthtest/handlers.clj
@@ -9,6 +9,11 @@
            [com.nimbusds.oauth2.sdk Scope]
            [com.nimbusds.oauth2.sdk ResponseType]
            [com.nimbusds.oauth2.sdk TokenRequest]
+           [com.nimbusds.oauth2.sdk TokenResponse]
+           [com.nimbusds.oauth2.sdk.auth ClientAuthentication]
+           [com.nimbusds.oauth2.sdk.auth ClientAuthenticationMethod]
+           [com.nimbusds.oauth2.sdk.auth ClientSecretBasic]
+           [com.nimbusds.oauth2.sdk.auth Secret]
            [com.nimbusds.oauth2.sdk.id ClientID]
            [com.nimbusds.oauth2.sdk.id State]
            [java.net URI]))
@@ -23,17 +28,21 @@
                           (State. "statefoo")
                           (Nonce. "noncebar")))
 
+(def ar (atom nil))
+
+
 (defn send [auth-req]
   (-> auth-req .toHTTPRequest .send))
 
 (defn make-auth-handler [client-id provider-uri redirect-uri scopes]
   (let [areq (auth-request client-id provider-uri redirect-uri scopes)
         code-uri (.toURI areq)]
+    (reset! ar areq)
     (fn [req]
       {:status 302
        :headers {"Location" code-uri}})))
 
-(defn make-code-handler [client-id token-uri redirect-uri]
+(defn make-code-handler [client-id client-secret token-uri redirect-uri]
   (fn [req]
     (println req)
     (println (r/request-url req))
@@ -41,10 +50,18 @@
       (println ares))
     (let [acode (AuthorizationCode. (-> req :params :code))
           grant (AuthorizationCodeGrant. acode (URI. redirect-uri))
+          clientauth (ClientSecretBasic. (ClientID. client-id)
+                                         (Secret. client-secret))
           treq (TokenRequest. (URI. token-uri)
-                              (ClientID. client-id)
-                              grant)]
-      (println treq))
+                              clientauth
+                              grant)
+          tres (TokenResponse/parse (-> treq .toHTTPRequest .send))]
+      (println "TREQ")
+      (println treq)
+      (println "TRES")
+      (println tres)
+      (println (-> tres .toHTTPResponse .getStatusCode))
+      (println (-> tres .toHTTPResponse .getContent)))
     {:status 200
      :headers {"Content-type" "text/plain"}
      :body "wurstschinken"}))
