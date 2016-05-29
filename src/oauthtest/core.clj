c91568ebdb1de8f5c5fe3387248b42d9d02bee89
Oliver Wegner <void1976@gmail.com>
2016-05-29 17:17:35 +0200

    working on tokenrequest

--- src/oauthtest/core.clj
+++ src/oauthtest/core.clj
@@ -7,13 +7,21 @@
 
 (defonce server (atom nil))
 
+(comment (defn get-env []
+           {:clientid (System/getenv "CLIENTID")
+            :clientsecret (System/getenv "CLIENTSECRET")
+            :provideruri (System/getenv "PROVIDERURI")
+            :redirecturi (System/getenv "REDIRECTURI")}))
+
 (defroutes auth-routes
   (context "/auth" []
            (GET "/"     [] (h/make-auth-handler "817304564499-tg8htvvunf7ds8rbp0jc30ijm20odo7k.apps.googleusercontent.com"
                                                 "https://accounts.google.com/o/oauth2/v2/auth"
                                                 "http://localhost:8899/auth/code"
                                                 ["openid" "email"]))
-           (GET "/code" [] h/code-handler)))
+           (GET "/code" [] (h/make-code-handler "817304564499-tg8htvvunf7ds8rbp0jc30ijm20odo7k.apps.googleusercontent.com"
+                                                "http://www.googleapis.com"
+                                                "http://localhost:8899/auth/code"))))
 
 (defn start []
   (reset! server (hs/run-server (site #'auth-routes)
--- src/oauthtest/handlers.clj
+++ src/oauthtest/handlers.clj
@@ -1,10 +1,14 @@
-(ns oauthtest.handlers
+[](ns oauthtest.handlers
   (:refer-clojure :rename {send send-clj})
+  (:require [ring.util.request :as r])
   (:import [com.nimbusds.openid.connect.sdk AuthenticationRequest]
            [com.nimbusds.openid.connect.sdk AuthenticationResponseParser]
            [com.nimbusds.openid.connect.sdk Nonce]
+           [com.nimbusds.oauth2.sdk AuthorizationCode]
+           [com.nimbusds.oauth2.sdk AuthorizationCodeGrant]
            [com.nimbusds.oauth2.sdk Scope]
            [com.nimbusds.oauth2.sdk ResponseType]
+           [com.nimbusds.oauth2.sdk TokenRequest]
            [com.nimbusds.oauth2.sdk.id ClientID]
            [com.nimbusds.oauth2.sdk.id State]
            [java.net URI]))
@@ -29,11 +33,21 @@
       {:status 302
        :headers {"Location" code-uri}})))
 
-(defn code-handler [req]
-  (println req)
-  {:status 200
-   :headers {"Content-type" "text/plain"}
-   :body "wurstschinken"})
+(defn make-code-handler [client-id token-uri redirect-uri]
+  (fn [req]
+    (println req)
+    (println (r/request-url req))
+    (let [ares (AuthenticationResponseParser/parse (URI. (r/request-url req)))]
+      (println ares))
+    (let [acode (AuthorizationCode. (-> req :params :code))
+          grant (AuthorizationCodeGrant. acode (URI. redirect-uri))
+          treq (TokenRequest. (URI. token-uri)
+                              (ClientID. client-id)
+                              grant)]
+      (println treq))
+    {:status 200
+     :headers {"Content-type" "text/plain"}
+     :body "wurstschinken"}))
 
 
 (comment (def ar1 (auth-request "817304564499-tg8htvvunf7ds8rbp0jc30ijm20odo7k.apps.googleusercontent.com"
