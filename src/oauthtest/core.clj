2230eca167064a586e1101ebc095c8adf491f7bd
Oliver Wegner <void1976@gmail.com>
2016-05-30 13:16:08 +0200

    adding some comments about where to get and how to set openid connect parameters

--- src/oauthtest/core.clj
+++ src/oauthtest/core.clj
@@ -5,14 +5,29 @@
             [compojure.handler :refer [site]]))
 
 
-(def conn-data {:client-id     "817304564499-tg8htvvunf7ds8rbp0jc30ijm20odo7k.apps.googleusercontent.com"
+(def conn-data {;;; client-id & client-secret: Your OP will give these to you after registration
+                ;;; (contrary to this example, you probably want to keep client-secret private, so
+                ;;; you'll probably want to get it from some config instead of hardcoding it in
+                ;;; your source code):
+                :client-id     "817304564499-tg8htvvunf7ds8rbp0jc30ijm20odo7k.apps.googleusercontent.com"
                 :client-secret "dFlqsKkhk0oA1se3zk5AUKYJ"
-                :provider-url  "https://accounts.google.com/o/oauth2/v2/auth"
+
+                ;;; redirect-url: Point this to the url you want users to be redirected to after they've
+                ;;;   completed authentication at the OP (note that you might have to tell your OP a list
+                ;;;   of valid redirect-urls):
                 :redirect-url  "http://localhost:8899/auth/code"
+                ;;; scopes: Set this to desired OpenID Connect scopes that your app needs to gain access to:
                 :scopes        ["openid" "email"]
+
+                ;;; Get these URLs from your OP. For the example of Google, there is a discovery document at
+                ;;;   https://accounts.google.com/.well-known/openid-configuration
+                ;;; For more info, you want to read your OP's docs, e.g.
+                ;;;   https://developers.google.com/identity/protocols/OpenIDConnect#discovery
                 :issuer        "https://accounts.google.com"
+                :provider-url  "https://accounts.google.com/o/oauth2/v2/auth"
                 :token-url     "https://www.googleapis.com/oauth2/v4/token"
-                :jwks-url      "https://www.googleapis.com/oauth2/v3/certs"})
+                :jwks-url      "https://www.googleapis.com/oauth2/v3/certs"
+                })
 
 
 (def server (atom nil))
